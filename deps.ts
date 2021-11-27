8908fb281eba9b0e1eaa1b6d2bd9bb672a5a87b7
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-11-27 13:34:17 +0100

    Create deps.ts

---
+++ deps.ts
@@ -0,0 +1,8 @@
+export * from "https://deno.land/std@0.105.0/fmt/colors.ts";
+export { Pool } from "https://deno.land/x/postgres@v0.11.2/mod.ts";
+export type { ConnectionOptions } from "https://deno.land/x/postgres@v0.11.2/mod.ts";
+export { AbstractMigration, ClientPostgreSQL } from "https://deno.land/x/nessie@2.0.4/mod.ts";
+export type { NessieConfig, PostgreSQLClientOptions } from "https://deno.land/x/nessie@2.0.4/mod.ts";
+export * as mammoth from "https://denopkg.com/itohatweb/mammoth@cae278d1e4deb0e9c4660e586492bddbd891880a/mod.ts";
+// @deno-types="https://denopkg.com/porsager/postgres@e2a8595d7aa8c3c838b83b9bca7b890c1707ad2c/types/index.d.ts"
+export { default as postgres } from "https://denopkg.com/porsager/postgres@e2a8595d7aa8c3c838b83b9bca7b890c1707ad2c/deno/lib/index.js";
